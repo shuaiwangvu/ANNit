2e0635ef4edc5bc8580cf34fab86aa042ab81abe
shuaiwangvu <shuai.wang.vu@gmail.com>
2020-06-21 15:50:43 +0200

    set up database: Entry and Choice

--- ANNit/ANNit/settings.py
+++ ANNit/ANNit/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'annotate.apps.AnnotateConfig',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Amsterdam'
 
 USE_I18N = True
 
